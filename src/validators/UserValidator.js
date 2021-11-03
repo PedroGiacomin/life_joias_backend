c952812539c100d08dfbd39d01a079e0e74d8fd5
Pedro <plsg2020.ufmg@gmail.com>
2021-11-03 18:35:27 -0300

    Reverti o commit

--- src/validators/UserValidator.js
+++ src/validators/UserValidator.js
@@ -31,17 +31,17 @@ module.exports = {
         }),
 
         [Segments.BODY]: Joi.object().keys({
-            user_nome:Joi.string().optional(),
-            user_email:Joi.string().optional(),
-            user_telefone:Joi.string().optional(),
-            user_nascimento:Joi.string().optional(),
-            user_cep:Joi.string().optional(),
-            user_rua:Joi.string().optional(),
-            user_numero:Joi.string().optional(),
-            user_bairro:Joi.string().optional(),
-            user_cidade:Joi.string().optional(),
-            user_estado:Joi.string().optional(),
-            user_senha:Joi.string().optional()
+            user_nome:Joi.string().required(),
+            user_email:Joi.string().required(),
+            user_telefone:Joi.string().required(),
+            user_nascimento:Joi.string().required(),
+            user_cep:Joi.string().required(),
+            user_rua:Joi.string().required(),
+            user_numero:Joi.string().required(),
+            user_bairro:Joi.string().required(),
+            user_cidade:Joi.string().required(),
+            user_estado:Joi.string().required(),
+            user_senha:Joi.string().required()
         })
         . min(1),
      }),
